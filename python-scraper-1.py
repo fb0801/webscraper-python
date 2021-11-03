e7f2449963f72702365b61559117b2b875481c82
Farhan <74499472+fb0801@users.noreply.github.com>
2021-11-03 21:22:47 +0000

    Add files via upload

--- python-scraper-1.py
+++ python-scraper-1.py
@@ -5,5 +5,8 @@ with open('index.html', 'r') as f:
     doc = BeautifulSoup(f, "html.parser")
 
 tags = doc.find_all('p')
+#doc.title
+#doc.find()
+#find_all('p')[0] get the first element
 
 print(tags)
