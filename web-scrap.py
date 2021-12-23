0133eb9474ce3f5a89a53227e042238d3eb222fd
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 20:38:20 +0000

    Add files via upload

--- web-scrap.py
+++ web-scrap.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 import requests
 
 
-gpu = input('what would you like to search  for? ') # input from user
+search_term = input('what would you like to search  for? ') # input from user
 
-url=f"https://www.newegg.com/global/uk-en/p/pl?d={gpu}&N=4131" #feeding the input to the url to find the result
+url=f"https://www.newegg.com/global/uk-en/p/pl?d={search_term}&N=4131" #feeding the input to the url to find the result
 page = requests.get(url).text
 doc= BeautifulSoup(page, 'html.parser')
 
@@ -15,9 +15,9 @@ page_text = doc.find(class_="list-tool-pagination-text").strong
 pages = int(str(page_text).split("/")[-2].split(">")[-1][:1])
 
 for page in range(1, pages + 1):
-    url=f"https://www.newegg.com/global/uk-en/p/pl?d={gpu}&N=4131&page={page}" #feeding the input to the url to find the result
+    url=f"https://www.newegg.com/global/uk-en/p/pl?d={search_term}&N=4131&page={page}" #feeding the input to the url to find the result
     page = requests.get(url).text
     doc= BeautifulSoup(page, 'html.parser')
-    items = doc.find_all(text=re.compile(gpu))
+    items = doc.find_all(text=re.compile(search_term))
 
 print(pages, "pages found")
