08271639a9dd66cb2c68c59c2126710cb6bbe157
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 20:24:56 +0000

    Add files via upload

--- web-scrap.py
+++ web-scrap.py
@@ -12,6 +12,6 @@ page = requests.get(url).text
 doc= BeautifulSoup(page, 'html.parser')
 
 page_text = doc.find(class_="list-tool-pagination-text").strong
-pages = str(page_text).split("/")[-2]
+pages = int(str(page_text).split("/")[-2].split(">")[-1][:1])
 
 print(pages)
