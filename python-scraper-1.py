fa7ad90e734b94c4218ef8ce76e953c81b4cb114
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 17:31:49 +0000

    Add files via upload

--- python-scraper-1.py
+++ python-scraper-1.py
@@ -1,13 +1,20 @@
 #import bs4
 #import BeautifulSoup
+from os import link
 import re
 from bs4 import BeautifulSoup
 with open('index.html', 'r') as f:
     doc = BeautifulSoup(f, "html.parser")
 
-tags = doc.find_all(text=re.compile('\$.*'))
+#tags = doc.find_all(text=re.compile('\$.*'))
 #doc.title
 #doc.find()
 #find_all('p')[0] get the first element
+tags = doc.find_all('p', type='text')
+for tag in tags:
+    tag['placeholder']='Im new hehe'
+
+with open("changed.html", "w") as file:
+    file.write(str(doc))
 
 print(tags)
