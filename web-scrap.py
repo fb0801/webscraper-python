4fdcd38d51cd7a11122d7925bc8370bb855408d7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 20:30:56 +0000

    Add files via upload

--- web-scrap.py
+++ web-scrap.py
@@ -14,7 +14,7 @@ doc= BeautifulSoup(page, 'html.parser')
 page_text = doc.find(class_="list-tool-pagination-text").strong
 pages = int(str(page_text).split("/")[-2].split(">")[-1][:1])
 
-for page in range(pages):
+for page in range(1, pages + 1):
     url=f"https://www.newegg.com/global/uk-en/p/pl?d={gpu}&N=4131&page={page}" #feeding the input to the url to find the result
     page = requests.get(url).text
     doc= BeautifulSoup(page, 'html.parser')
