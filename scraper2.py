1763517e76b4a27008daaf75d89beefa888d17d4
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 17:36:12 +0000

    Add files via upload

---
+++ scraper2.py
@@ -0,0 +1,13 @@
+#import BeautifulSoup
+from os import link
+import re
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://coinmarketcap.com/"
+result  = requests.get(url).text
+doc = BeautifulSoup(result, "html.parser")
+
+tbody = doc.tbody
+trs = tbody.contents
+print(trs[1].previous_sibling)
