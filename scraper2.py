bc1f30055ae9146ad0369879e2e4bd706a6eeb38
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 17:50:17 +0000

    Add files via upload

--- scraper2.py
+++ scraper2.py
@@ -1,5 +1,5 @@
 #import BeautifulSoup
-from os import link
+
 import re
 from bs4 import BeautifulSoup
 import requests
@@ -12,7 +12,10 @@ tbody = doc.tbody
 trs = tbody.contents
 prices = {}
 
-for tr in trs[10]:
+for tr in trs[:10]:
     name, price = tr.contents[2:4]
     fixed_name = name.p.string
-    print(price)
+    fixed_price = price.a.string    
+    
+    prices[fixed_name] = fixed_price
+print(prices)
