8d37767ba4fd1357bd4f42143e60a906f91804d0
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-23 17:20:08 +0000

    Add files via upload

--- python-scraper-1.py
+++ python-scraper-1.py
@@ -1,10 +1,11 @@
 #import bs4
 #import BeautifulSoup
+import re
 from bs4 import BeautifulSoup
 with open('index.html', 'r') as f:
     doc = BeautifulSoup(f, "html.parser")
 
-tags = doc.find_all(class_="dot")
+tags = doc.find_all(text=re.compile('\$.*'))
 #doc.title
 #doc.find()
 #find_all('p')[0] get the first element
